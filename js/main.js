97c0a0ef633b37b6af916f55468e9ee86fc9ea06
FelixT11 <felix@goldtalla.de>
2021-04-15 22:24:02 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper')
 
-portfolioItems.forEach(portfolioItems => {
-    portfolioItems.addEventListener('mouseover', () => {
-        portfolioItems.childNodes[1].classList.add('img-darken');
+portfolioItems.forEach(portfolioItem => {
+    portfolioItem.addEventListener('mousedown', () => {
+        portfolioItem.childNodes[1].classList.add('img-darken');
+        console.log(portfolioItem.childNodes[2]);
     })
-    portfolioItems.addEventListener('mouseout', () => {
-        portfolioItems.childNodes[1].classList.remove('img-darken');
+    portfolioItem.addEventListener('mouseout', () => {
+        portfolioItem.childNodes[1].classList.remove('img-darken');
+    })
+})
+
+
+const titles = document.querySelectorAll('.title')
+
+titles.forEach(title => {
+    title.addEventListener('mousedown', () => {
+        title.childNodes[1].classList.add('subtitle-active');
+        title.childNodes[2].classList.add('text-active');
+    })
+    title.addEventListener('mouseout', () => {
+        title.childNodes[1].classList.remove('subtitle-active');
+        title.childNodes[2].classList.remove('text-active');
     })
 })
