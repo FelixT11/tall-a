77c0108a6a9cd601822b53de3babe051479298f4
ahwelgemoed <arno.welgemoed@outlook.com>
2021-04-16 18:37:04 +0200

    Arno Fixes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,128 @@
-const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper')
+/**
+ *  This is an Array o Objects, Each object is one Shirt.
+ * This makes it all bit easier to maintain. As we Loop over the array and generate the HTML
+ * If you dont get it just delete it
+ */
 
-portfolioItems.forEach(portfolioItem => {
-    portfolioItem.addEventListener('mousedown', () => {
-        portfolioItem.childNodes[1].classList.add('img-darken');
-        console.log(portfolioItem.childNodes[2]);
-    })
-    portfolioItem.addEventListener('mouseout', () => {
-        portfolioItem.childNodes[1].classList.remove('img-darken');
-    })
-})
+const felixShirts = [
+  {
+    image: "images/tshirt_01.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_02.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_03.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_04.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_05.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_06.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_07.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_08.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_09.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_10.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_11.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_12.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_13.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_14.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+  {
+    image: "images/tshirt_15.jpg",
+    subtitle: "JMB, 30€",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur.",
+  },
+];
 
+const shirtDiv = document.getElementById("portfolio-items-wrapper");
 
-const titles = document.querySelectorAll('.title')
+shirtDiv.innerHTML = felixShirts
+  .map((shirt) => {
+    console.log(`shirt`, shirt);
+    return `<div class="portfolio-item-wrapper">
+        <div class="tshirt-img" style="background-image: url(${shirt.image})"></div>
+        <div class="title">
+            <h3 class="subtitle">${shirt.subtitle}</h3>
+            <p class="text">${shirt.text}</p>
+        </div>
+    </div>
+`;
+  })
+  .join("");
 
-titles.forEach(title => {
-    title.addEventListener('mousedown', () => {
-        title.childNodes[1].classList.add('subtitle-active');
-        title.childNodes[2].classList.add('text-active');
-    })
-    title.addEventListener('mouseout', () => {
-        title.childNodes[1].classList.remove('subtitle-active');
-        title.childNodes[2].classList.remove('text-active');
-    })
-})
+/**
+ *  This makes the hover and Click functions work
+ */
+const portfolioItems = document.querySelectorAll(".portfolio-item-wrapper");
+
+portfolioItems.forEach((portfolioItem) => {
+  portfolioItem.addEventListener("click", () => {
+    portfolioItem.classList.add("img-darken", "show-text");
+  });
+  portfolioItem.addEventListener("mouseleave", () => {
+    portfolioItem.classList.remove("img-darken", "show-text");
+  });
+});
